714b71bb0ef493c04630f78b4be12321da58cf21
RzHridoy <rz.hridoy003@gmail.com>
2025-03-21 22:17:54 +0100

    first commit

---
+++ class1.py
@@ -0,0 +1,13 @@
+print("Beginning of the NumPY Course \n")
+
+import numpy as np
+
+list1 = [1, 2, 3, 4, 5]
+print(list1)
+
+np1 = np.array([[1, 2, 3],
+               [2, 3, 2]])
+print(np1)
+print(np1.shape)
+print(np1.ndim)
+print(np1.itemsize)
---
+++ class2.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+np1 = np.array([1, 2, 3, 6, 5, 1])
+print(np1)
+print(np1.shape)
+print(np1.size)
+
+#slicing
+print(np1[0:4])
