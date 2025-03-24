2bec1a30e3f146f5734980546df9c6d9d1a3749c
RzHridoy <rz.hridoy003@gmail.com>
2025-03-24 23:48:48 +0100

    first commit

---
+++ class3.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+np1 = np.array([1, 2, 3, 4])
+print(f"Original NP1 is {np1}")
+
+#create a view
+np2 = np1.view()
+print(f"Original NP2 is {np2}")
+
+#create view after changing np1 then np2 array
+'''np1[2] = 5
+print(f"Change NP1 is {np1}")
+print(f"Original NP2 is {np2}")'''
+
+np2[0] = 0
+print(f"Original NP1 is {np1}")
+print(f"Change NP2 is {np2}")
